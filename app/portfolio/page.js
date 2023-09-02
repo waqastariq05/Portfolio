e60e138c471c0747da3198d606166a5cf74258e2
Waqas <waqastariq274@gmail.com>
2023-09-03 00:56:06 +0500

    Fix bug

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -5,7 +5,7 @@ const Product = dynamic(() => import('../../components/Product'))
 
 const page = () => {
     return (
-        <div className="mx-auto md:px-20 sm:px-10 px-5">
+        <div className="container mx-auto md:px-20 sm:px-10 px-5">
             <Transition />
             <div className="flex justify-center flex-col gap-10 h-fit md:pb-10 pb-20 md:pt-20 pt-3">
                 <Header title="my portfolio" text="Visit My Portfolio & Keep Your Feedback" />
