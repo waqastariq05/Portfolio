4d6bf17f3df20891563dce1c0b4dad561c58d131
Waqas <waqastariq274@gmail.com>
2023-09-02 17:12:09 +0500

    Fixing Accessibility Issues

--- components/NavTab.js
+++ components/NavTab.js
@@ -6,7 +6,7 @@ const NavTab = (props) => {
 
     return (
         <>
-            <span className={`sm:px-6 px-4 py-3 w-auto justify-center sm:justify-start border-b-2 title-base font-medium inline-flex items-center leading-none  tracking-wider rounded-t capitalize hover:text-orange-600 ${index === itemIndex && 'bg-gray-100 border-orange-500 text-black'} transition-all duration-200`} key={itemIndex} onClick={() => setIndex(itemIndex)}>{item.title}</span>
+            <span className={`sm:px-6 px-4 py-3 w-auto justify-center sm:justify-start border-b-2 title-base font-medium inline-flex items-center leading-none  tracking-wider rounded-t capitalize hover:text-orange-600 ${index === itemIndex && 'bg-gray-100 border-orange-500 text-orange-800'} transition-all duration-200`} key={itemIndex} onClick={() => setIndex(itemIndex)}>{item.title}</span>
         </>
     )
 }
